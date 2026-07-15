94cb6b07d32bc7ae743b3b0985739bde7c1848ec
JinVibe <loonaticvibe2.11@gmail.com>
2026-07-16 01:10:37 +0900

    Initial commit

---
+++ src/test/java/jin/project/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package jin.project.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
