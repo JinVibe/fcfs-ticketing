0e2551997b19f0cd025bb216c8cb36c9a4ff1b81
JinVibe <loonaticvibe2.11@gmail.com>
2026-07-17 23:58:40 +0900

    feat: 비관적 락 대기 시간 제한

--- src/main/java/jin/project/ticketing/Domain/TicketRepository.java
+++ src/main/java/jin/project/ticketing/Domain/TicketRepository.java
@@ -1,16 +1,19 @@
 package jin.project.ticketing.Domain;
 
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Lock(LockModeType.PESSIMISTIC_WRITE) // 비관적 락 추가
+    @QueryHints(@QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000"))
     @Query("select t from Ticket t where t.id = :ticketId")
     Optional<Ticket> findByIdWithPessimisticLock(@Param("ticketId") Long ticketId);
 }
